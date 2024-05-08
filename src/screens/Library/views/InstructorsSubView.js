2046c082d43cb943e3e18bd09a30d89f7e926059
ngochan <20520484@gm.uit.edu.vn>
2024-05-09 05:55:02 +0700

    create 4 SubView for 4 TopTabNavigator in Library

---
+++ src/screens/Library/views/InstructorsSubView.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const InstructorsSubView = () => {
+    return(
+        <View>
+            <Text>Instructor Sub View</Text>
+        </View>
+    )
+}
+
+export default InstructorsSubView;
---
+++ src/screens/Library/views/ProgramsSubView.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const ProgramsSubView = () => {
+    return(
+        <View>
+            <Text>Program Sub View</Text>
+        </View>
+    )
+}
+
+export default ProgramsSubView;

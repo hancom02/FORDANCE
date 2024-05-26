945bf0756f7a3749e18873bc94db14f8aa918d75
ngochan <20520484@gm.uit.edu.vn>
2024-05-27 04:02:22 +0700

    use Redux

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,17 +1,16 @@
 import HomeContainer from "./HomeContainer";
 
-//import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 const Home = (props) => {
-    //const dispatch = useDispatch();
+    const dispatch = useDispatch();
     const {
-        navigation,
-        //dispatch,
-        
+        navigation,        
     } = props;
 
     const propsContainer = {
+        dispatch,
         navigation,
 
     };
